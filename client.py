fc27e4a72c74f5b0020bdbd5c25c2f53313a88ee
Edgar Samudio <esamudio@Edgars-MacBook-Pro.local>
2016-12-10 16:41:04 -0500

    altered skeleton to account for whether user is logged in or not. added corresponding feedback for user

--- client.py
+++ client.py
@@ -3,35 +3,54 @@ import socket
 import sys
 
 HOST, PORT = "localhost", 9999
+INVALID_INPUT = "{} is not a proper instruction. Please try again\n"
+ALREADY_LOGGED_IN = "Already logged in as {}. Please log out if you want to log in again.\n"
+
 
 # Create a socket (SOCK_STREAM means a TCP socket)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect((HOST, PORT))
+    username = None
+
     while(True):
         # Connect to server and send data
         data = sys.stdin.readline()
         instruction = data.split(' ')[0]
         if instruction == 'login':
-            pass
+            if is_logged_in(username):
+                print(ALREADY_LOGGED_IN.format(username))
+            else:
+                login(username, data)
         elif instruction == 'help':
-            pass
+            print(client_functions.HELP)
+            continue
         elif instruction == 'ag':
-            pass
+            if is_logged_in(username):
+                pass
+            else:
+                not_logged_in()
         elif instruction == 'sg':
-            pass
+            if is_logged_in(username):
+                pass
+            else:
+                not_logged_in()
         elif instruction == 'rg':
-            pass
+            if is_logged_in(username):
+                pass
+            else:
+                not_logged_in()
         elif instruction == 'logout':
-            pass
+            if is_logged_in(username):
+                username = None
+            else:
+                not_logged_in()
+        else:
+            print(INVALID_INPUT.format(data))
         
         try:
-            sock.sendall(bytes(data + "\n", "utf-8"))
+            sock.sendall(bytes(response + "\n", "utf-8"))
         
             # Receive data from the server
             received = str(sock.recv(1024), "utf-8")
         except SocketError as e:
             print(e)
-
-
-        print("Sent:     {}".format(data))
-        print("Received: {}".format(received))
