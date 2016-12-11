c5b3db80e9bcd0254b880e9c162e1f7ccedb5695
Isaac Ramos <isaacd.ramosny@gmail.com>
2016-12-10 21:07:50 -0500

    handle for thread race conditions.

--- database.py
+++ database.py
@@ -5,15 +5,15 @@ from json import dumps, load
 
 hlpdb = 'data/data.json'
 writingLock = {}
+readingLock = {}
 
-
-def get_categories():
-    with open(hlpdb) as f:
-        return load(f)
 
 
 def get_group(group):
+    while writingLock[hlpdb]:
+        time.sleep(1)
     with open(hlpdb) as f:
+        readingLock[hlpdb]= True
         db = load(f)
         groups = db['discussion_groups']
 
