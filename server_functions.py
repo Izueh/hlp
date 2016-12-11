0568fb05a6c426919cc13ca2612c8fa23468ee37
Edgar Samudio <esamudio@Edgars-MacBook-Pro.local>
2016-12-11 00:48:46 -0500

    using ag as template for sg

--- server_functions.py
+++ server_functions.py
@@ -30,3 +30,18 @@ def ag(usergroups):
         data +='\n'
     data.rstrip()
     return data
+
+def sg(usergroups):
+    usergroups= usergroups.split(' ')
+    ugroups = [int(g) for g in usergroups[3].split('\5')]
+    data = ''
+    groups = get_group()
+    for i in range(int(usergroups[1]),int(usergroups[2])):
+        if i > len(groups):
+            break
+        if groups[i]['group_id'] in ugroups:
+	        data += '%d. (%s) %s' % \
+	                (groups[i]['group_id'],'s' if groups[i]['group_id'] in ugroups else ' ', groups[i]['group_id'])
+	        data +='\n'
+    data.rstrip()
+    return data
