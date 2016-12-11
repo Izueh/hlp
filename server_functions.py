c9ac94f57a4c603867684855b527172884aa93e3
izueh <bldkaiser@gmail.com>
2016-12-10 23:53:37 -0500

    implemented ag

--- server_functions.py
+++ server_functions.py
@@ -5,13 +5,23 @@ hlpdb = 'data/data.json'
 writingLock = {}
 readingLock= {}
 
-def get_group(group):
+def get_group():
     while writingLock[hlpdb]:
         time.sleep(1)
-
     with open(hlpdb) as f:
         db = load(f)
         readingLock[hlpdb] = True
         groups = db['discussion_groups']
 
     readingLock.pop(hlpdb)
+    return groups
+
+
+def ag(usergroups):
+    usergroups= usergroups.split(' ')
+    ugroups = [int(g) for g in usergroups[2].split('\5')]
+
+    groups = get_group()
+    data = '\n'.join('%d. (%s) %s ' % (g['group_id'],'s' if g['group_id'] in ugroups else ' ' , g['title'] ) for g in groups)
+
+    return data
