660a60c0f4af264f52431f1bb28ff6d1676e0362
izueh <bldkaiser@gmail.com>
2016-12-10 23:17:50 -0500

    refactoring

---
+++ server_functions.py
@@ -0,0 +1,17 @@
+import time
+from json import dumps, load
+
+hlpdb = 'data/data.json'
+writingLock = {}
+readingLock= {}
+
+def get_group(group):
+    while writingLock[hlpdb]:
+        time.sleep(1)
+
+    with open(hlpdb) as f:
+        db = load(f)
+        readingLock[hlpdb] = True
+        groups = db['discussion_groups']
+
+    readingLock.pop(hlpdb)
